c28009306449f7143d9c7ae176345a22db0f373d
Julius de Bruijn <bruijn@prisma.io>
2019-12-05 17:14:13 +0100

    Revert uid/gid

--- .buildkite/docker.sh
+++ .buildkite/docker.sh
@@ -6,4 +6,4 @@ chmod 0644 hetzner_key
 DATABASE=$1
 
 docker pull prismagraphql/build:test
-docker run -u $(id -u):$(id -g) -v $(pwd):/build -w /build -e ELASTIC_USER=$ELASTIC_USER -e ELASTIC_PW=$ELASTIC_PW -e RUST_BACKTRACE=$RUST_BACKTRACE prismagraphql/build:test /build/.buildkite/$DATABASE.sh
+docker run -v $(pwd):/build -w /build -e ELASTIC_USER=$ELASTIC_USER -e ELASTIC_PW=$ELASTIC_PW -e RUST_BACKTRACE=$RUST_BACKTRACE prismagraphql/build:test /build/.buildkite/$DATABASE.sh
