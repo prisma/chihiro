ad3b0229bf79be92ee89facb35d9b7142f1d5440
Julius de Bruijn <bruijn@prisma.io>
2019-12-05 14:14:31 +0100

    Slash dash

--- .buildkite/postgres.sh
+++ .buildkite/postgres.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cargo run --release -- setup --private-key hetzner_key --user prisma bm-app-psql.prisma.io:22
-cargo run --release -- bench --metrics-database prisma_benchmark --validate --prisma-url http://bm-app-psql.prisma.io/chinook --query-file test_run.toml
+cargo run --release -- bench --metrics-database prisma_benchmark --validate --prisma-url http://bm-app-psql.prisma.io/chinook/ --query-file test_run.toml
