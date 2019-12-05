72a6f18dd7d17c17ba8b1050dcf8208318672059
Julius de Bruijn <bruijn@prisma.io>
2019-12-05 16:02:06 +0100

    Benchmarks should run in hetzner agents

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -4,8 +4,12 @@ echo "steps:
   - label: \":postgres: Benchmark Postgres\"
     command: ./.buildkite/docker.sh postgres
     branches: master
+    agents:
+        queue: benchmark
 
   - label: \":mysql: Benchmark MySQL\"
     command: ./.buildkite/docker.sh mysql
     branches: master
+    agents:
+        queue: benchmark
 " | buildkite-agent pipeline upload
