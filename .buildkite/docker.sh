0f88364ea244184526a8dcfd420e01942d870eac
Julius de Bruijn <bruijn@prisma.io>
2019-12-09 17:23:39 +0100

    Prune docker images before starting

--- .buildkite/docker.sh
+++ .buildkite/docker.sh
@@ -5,5 +5,7 @@ chmod 0644 hetzner_key
 
 DATABASE=$1
 
+(docker kill $(docker ps -aq); docker rm $(docker ps -aq)) || :
+docker system prune -f
 docker pull prismagraphql/build:test
 docker run -v $(pwd):/build -w /build -e ELASTIC_USER=$ELASTIC_USER -e ELASTIC_PW=$ELASTIC_PW -e RUST_BACKTRACE=$RUST_BACKTRACE prismagraphql/build:test /build/.buildkite/$DATABASE.sh
