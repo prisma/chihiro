7be895bcaf61625dfa6ba34a3e3fe7cfb78e83b6
Julius de Bruijn <bruijn@prisma.io>
2019-12-16 14:02:43 +0100

    Remove Hasura from tests

--- .buildkite/postgres.sh
+++ .buildkite/postgres.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 cargo run --release -- setup --private-key hetzner_key --user prisma bm-app-psql.prisma.io:22
-cargo run --release -- bench --metrics-database prisma_benchmark --validate --endpoint-url http://bm-app-psql.prisma.io/hasura/v1/graphql/ --query-file hasura.toml --endpoint-type hasura
 cargo run --release -- bench --metrics-database prisma_benchmark --validate --endpoint-url http://bm-app-psql.prisma.io/sql_load_test/ --query-file sql_load_test.toml
 cargo run --release -- bench --metrics-database prisma_benchmark --validate --endpoint-url http://bm-app-psql.prisma.io/chinook/ --query-file chinook.toml
