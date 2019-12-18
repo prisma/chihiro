c1afe7746bb2ce21d0bb8bc5f6969fa74be03474
Julius de Bruijn <bruijn@prisma.io>
2019-12-18 13:11:26 +0100

    Remove chinook queries from mysql run

--- .buildkite/mysql.sh
+++ .buildkite/mysql.sh
@@ -2,4 +2,3 @@
 
 cargo run --release -- setup --private-key hetzner_key --user prisma bm-app-mysql.prisma.io:22
 cargo run --release -- bench --metrics-database prisma_benchmark --validate --endpoint-url http://bm-app-mysql.prisma.io/sql_load_test/ --query-file sql_load_test.toml
-cargo run --release -- bench --metrics-database prisma_benchmark --validate --endpoint-url http://bm-app-mysql.prisma.io/chinook/ --query-file chinook.toml
